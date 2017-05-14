e3f43a49886665211fb2f56480b0b7b74445f764
romainba <romainba@dfsdf.com>
2017-05-14 13:08:36 +0200

    added daemonize

---
+++ target/daemonize.c
@@ -0,0 +1,114 @@
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "util.h"
+#include "daemonize.h"
+
+static char pid_file_name[] = "switch-control.pid";
+static int pid_fd = -1;
+
+/**
+ * \brief Callback function for handling signals.
+ * \param	sig	identifier of signal
+ */
+void handle_signal(int sig)
+{
+	if (sig == SIGINT) {
+		INFO("Debug: stopping daemon ...\n");
+		/* Unlock and close lockfile */
+		if (pid_fd != -1) {
+			lockf(pid_fd, F_ULOCK, 0);
+			close(pid_fd);
+		}
+		/* Try to delete lockfile */
+		if (pid_file_name != NULL) {
+			unlink(pid_file_name);
+		}
+		//running = 0;
+		/* Reset signal handling to default behavior */
+		signal(SIGINT, SIG_DFL);
+	} else if (sig == SIGHUP) {
+		INFO("Debug: reloading daemon config file ...\n");
+		//read_conf_file(1);
+	} else if (sig == SIGCHLD) {
+	  	INFO("Debug: received SIGCHLD signal\n");
+	}
+}
+
+/**
+ * \brief This function will daemonize this app
+ */
+void daemonize()
+{
+	pid_t pid = fork();
+	int fd;
+
+	if (pid < 0) {
+		exit(EXIT_FAILURE);
+	}
+	if (pid > 0) {
+		exit(EXIT_SUCCESS);
+	}
+
+	/* On success: The child process becomes session leader */
+	if (setsid() < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	/* Ignore signal sent from child to parent process */
+	signal(SIGCHLD, SIG_IGN);
+
+	/* Fork off for the second time*/
+	pid = fork();
+
+	/* An error occurred */
+	if (pid < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	/* Success: Let the parent terminate */
+	if (pid > 0) {
+		exit(EXIT_SUCCESS);
+	}
+
+	/* Set new file permissions */
+	//umask(0);
+
+	/* Change the working directory to the root directory */
+	/* or another appropriated directory */
+	chdir("/");
+
+	/* Close all open file descriptors */
+	for (fd = sysconf(_SC_OPEN_MAX); fd > 0; fd--) {
+		close(fd);
+	}
+
+	/* Reopen stdin (fd = 0), stdout (fd = 1), stderr (fd = 2) */
+	stdin = fopen("/dev/null", "r");
+	stdout = fopen("/dev/null", "w+");
+	stderr = fopen("/dev/null", "w+");
+
+	/* Try to write PID of daemon to lockfile */
+	if (pid_file_name != NULL)
+	{
+		char str[256];
+		pid_fd = open(pid_file_name, O_RDWR|O_CREAT, 0640);
+		if (pid_fd < 0) {
+			/* Can't open lockfile */
+			exit(EXIT_FAILURE);
+		}
+		if (lockf(pid_fd, F_TLOCK, 0) < 0) {
+			/* Can't lock file */
+			exit(EXIT_FAILURE);
+		}
+		/* Get current PID */
+		sprintf(str, "%d\n", getpid());
+		/* Write PID to lockfile */
+		write(pid_fd, str, strlen(str));
+	}
+}
---
+++ target/daemonize.h
@@ -0,0 +1,7 @@
+#ifndef __DAEMONIZE_H__
+#define __DAEMONIZE_H__
+
+void handle_signal(int sig);
+void daemonize(void);
+
+#endif
